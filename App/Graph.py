82a3ef6f390d5a09f4a7402fdfdea5bde9451794
Glas42 <145870870+OleFranz@users.noreply.github.com>
2025-04-13 01:59:49 +0200

    Graph improvements

--- App/Graph.py
+++ App/Graph.py
@@ -74,48 +74,57 @@ def Update():
             MaxY = MaxY + (MaxY - MinY) * 0.1
 
             MinY = 0 if MinY < 0 else MinY
+            MaxX += 1 if MaxX == MinX else 0
 
-            XAxisScale = 5
-            XAxisMax = MinX + ((len(Variables.GraphContent[0][2]) + XAxisScale - 1) // XAxisScale) * XAxisScale
-            YAxisScale = 5
+            XAxisScale = max(1, min(5, MaxX - 1))
+            XAxisMax = MinX + ((MaxX + XAxisScale - 2) // XAxisScale) * XAxisScale
+            YAxisScale = max(5, round(Variables.GraphZoom * 5))
 
             for i in range(XAxisScale + 1):
                 FloatX = i / XAxisScale
                 X, Y = ConvertToImageCoordinate(FloatX, 1)
-                cv2.line(Frame, ConvertToImageCoordinate(FloatX, 0), ConvertToImageCoordinate(FloatX, 1), (180, 180, 180) if FloatX == 0 else (50, 50, 50), 1)
-                cv2.line(Frame, (X, Y - 3), (X, Y + 3), (180, 180, 180), 1)
-                Text, Fontscale, Thickness, Width, Height = GetTextSize(str(int(MinX + (XAxisMax - MinX) * FloatX)), 1000, 10)
-                cv2.putText(Frame,
-                            Text,
-                            (round(X - Width / 2), round(Y + Height * 1.5)),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            Fontscale,
-                            (180, 180, 180),
-                            Thickness,
-                            cv2.LINE_AA)
+                if -10 <= X <= Variables.Graph.shape[1] + 9:
+                    cv2.line(Frame, ConvertToImageCoordinate(FloatX, 0), ConvertToImageCoordinate(FloatX, 1), (180, 180, 180) if FloatX == 0 else (50, 50, 50), 1)
+                    cv2.line(Frame, (X, Y - 3), (X, Y + 3), (180, 180, 180), 1)
+                    Text, Fontscale, Thickness, Width, Height = GetTextSize(f"{int(MinX + (XAxisMax - MinX) * FloatX):,}".replace(",", "."), 1000, 10)
+                    Y = round(Y + Height * 1.5)
+                    if Y >= Variables.Graph.shape[0] - 2:
+                        Y = Variables.Graph.shape[0] - 2
+                    cv2.putText(Frame,
+                                Text,
+                                (round(X - Width / 2), Y),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                Fontscale,
+                                (180, 180, 180),
+                                Thickness,
+                                cv2.LINE_AA)
 
             for i in range(YAxisScale + 1):
                 FloatY = 1 - i / YAxisScale
                 X, Y = ConvertToImageCoordinate(0, FloatY)
-                cv2.line(Frame, ConvertToImageCoordinate(0, FloatY), ConvertToImageCoordinate(1, FloatY), (180, 180, 180) if FloatY == 1 else (50, 50, 50), 1)
-                cv2.line(Frame, (X - 3, Y), (X + 3, Y), (180, 180, 180), 1)
-                Text, Fontscale, Thickness, Width, Height = GetTextSize(str(round(MaxY - (MaxY - MinY) * FloatY, 4)), 1000, 10)
-                cv2.putText(Frame,
-                            Text,
-                            (round(X - Width * 1.1), round(Y + Height / 2)),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            Fontscale,
-                            (180, 180, 180),
-                            Thickness,
-                            cv2.LINE_AA)
+                if -10 <= Y <= Variables.Graph.shape[0] + 9:
+                    cv2.line(Frame, ConvertToImageCoordinate(0, FloatY), ConvertToImageCoordinate(1, FloatY), (180, 180, 180) if FloatY == 1 else (50, 50, 50), 1)
+                    cv2.line(Frame, (X - 3, Y), (X + 3, Y), (180, 180, 180), 1)
+                    Text, Fontscale, Thickness, Width, Height = GetTextSize(f"{round(MaxY - (MaxY - MinY) * FloatY, max(2, min(9, round(3 * (Variables.GraphZoom / 100) * 100)))):,}".replace(",", "#").replace(".", ",").replace("#", "."), 1000, 10)
+                    X = round(X - Width * 1.1)
+                    if X < 1:
+                        X = 1
+                    cv2.putText(Frame,
+                                Text,
+                                (X, round(Y + Height / 2)),
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                Fontscale,
+                                (180, 180, 180),
+                                Thickness,
+                                cv2.LINE_AA)
 
             for Graph in Variables.GraphContent:
                 if Variables.Graphs[Graph[0]]["Show"]:
                     LastPoint = None
                     for X in range(len(Graph[2])):
-                        X, Y = ConvertToImageCoordinate((Graph[2][X][0] - MinX) / (XAxisMax - MinX), 1 - Graph[2][X][1] / MaxY)
+                        X, Y = ConvertToImageCoordinate((Graph[2][X][0] - MinX) / (XAxisMax - MinX) if XAxisMax - MinX != 0 else 0, 1 - Graph[2][X][1] / MaxY)
                         if len(Graph[2]) == 1:
-                            cv2.circle(Frame, (X, Y), 1, Graph[1], 1, cv2.LINE_AA)
+                            cv2.circle(Frame, (X, Y), 1, Graph[1], 2, cv2.LINE_AA)
                         elif LastPoint != None:
                             cv2.line(Frame, LastPoint, (X, Y), Graph[1], 1, cv2.LINE_AA)
                         LastPoint = (X, Y)
